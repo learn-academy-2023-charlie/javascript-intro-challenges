a3146819c0a745dfe35d5a814ec50612e0c5895b
Tmacintyre <Tucker.w.macintyre@gmail.com>
2023-04-19 19:26:34 -0400

    third update

--- conditionals-tucker-justin.js
+++ conditionals-tucker-justin.js
@@ -42,15 +42,48 @@ if(price <= 100){
     console.log("in budget")
 } else {
     console.log("not in budget")
-}
-// in budget
+} // in budget
 
 // Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+const food = "hungry"
 
+if(food === "hungry"){
+    console.log("eat food")
+} else{
+    console.log("keep coding")
+} // eat food
 
 // Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+ let trafficLight = ("green");
+
+ if(trafficLight === "green"){
+    console.log("go")
+ } else if(trafficLight === "yellow"){
+    console.log("slow down")
+ } else{
+    console.log("stop")
+ }
+
 
 // Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+const numOne = -10
+const numTwo = 5
+
+if(numOne === numTwo){
+    console.log("the numbers are the same")
+} else if(numOne > numTwo){
+    console.log(numOne)
+} else{
+    console.log(numTwo)
+}
 
 // Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
-
+const numThree = 19
+if(numThree === 0){
+    console.log("is zero")
+}
+  else if(numThree % 2 === 0){
+    console.log("even")
+} else if(numThree % 2 === 1){
+    console.log("is odd")
+} 
