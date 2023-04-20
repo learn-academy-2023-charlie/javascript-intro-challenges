e6d3da2a30debc95c969e79ff952378e3a2f65ba
m3vance <m3vance@gmail.com>
2023-04-20 16:37:59 -0700

    vance and catalino loops challenge

--- loops-vance-catlino.js
+++ loops-vance-catlino.js
@@ -1,10 +1,22 @@
 // Loops Challenges 4/20/23
 
 // Create a for loop that logs each number from 1 - 20.
-for (let i = 1; i < 21; i++)
-console.log(i)
+// for (let i = 1; i < 21; i++)
+// console.log(i)
 
-// Create a for loop that logs every other number from 1 - 20.
+// // Create a for loop that logs every other number from 1 - 20.
+// for (let i = 1; i < 21; i = i+2)
+// console.log(i)
+
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
+// for (let i = 1; i < 21; i++)
+// console.log(i*3)
+
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+for (let i = 2; i < 21; i=i+2)
+console.log("odd", i)
+
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+
+
+
