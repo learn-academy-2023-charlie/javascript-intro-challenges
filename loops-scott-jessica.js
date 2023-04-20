538e8610cd88c82d17932a896ec5f39c6a435845
jricks86 <jricks86@gmail.com>
2023-04-20 16:20:59 -0700

    update

--- loops-scott-jessica.js
+++ loops-scott-jessica.js
@@ -12,7 +12,7 @@ for (let i = 1; i <= 20; i += 2) {
 
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
 for (let i = 1; i <= 20; i++) {
-    console.log(i * 3);
+    // console.log(i * 3);
     
 }
 
@@ -21,7 +21,7 @@ for (let i = 1; i <= 20; i++) {
     if (i % 2 === 0) {
       console.log(i);
     } else {
-      console.log("ODD");
+    //   console.log("ODD");
     }
   }
 
@@ -30,9 +30,22 @@ for (let i = 1; i <= 20; i++) {
 
 // Consider this variable:
 
-// const nums = [3, 57, -9, 20, 67]
+const nums = [3, 57, -9, 20, 67]
 
 // Create the code that will log the largest number from the array.
+const nums = [3, 57, -9, 20, 67];
+
+let largestNum = nums[0];
+
+for (let i = 1; i < nums.length; i++) {
+  if (nums[i] > largestNum) {
+    largestNum = nums[i];
+  }
+}
+
+console.log(largestNum);
+
+
 // Create the code that will log the smallest number from the array.
 // Create the code that will log the remainder of each number when divided by 2. Expected output: 1, 1, -1, 0, 1
 // Consider this variable:
