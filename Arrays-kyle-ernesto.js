e45491f6d01a8482015d3d5985bdf86130f93a15
kyloh1996 <kyloh1996@gmail.com>
2023-04-20 16:01:50 -0500

    thrid trial

--- Arrays-kyle-ernesto.js
+++ Arrays-kyle-ernesto.js
@@ -1,27 +1,66 @@
-var groceryList = ["chips", "dip", "cookies"]
+// // var groceryList = ["chips", "dip", "cookies"]
 
 
-// Write the code that will add "soda" to the end of the original array.
+// // // Write the code that will add "soda" to the end of the original array.
 
-// groceryList.push("soda")
-// console.log(groceryList)
-// output: [ 'chips', 'dip', 'cookies', 'soda' ]
+// // // groceryList.push("soda")
+// // // console.log(groceryList)
+// // // output: [ 'chips', 'dip', 'cookies', 'soda' ]
 
-// Write the code that will add "granola" to the end of array without altering the original array.
+// // // Write the code that will add "granola" to the end of array without altering the original array.
 
-var myOtherArray = ["granola"]
-myOtherArray.concat(groceryList)
-// console.log(groceryList.concat(myOtherArray))
-// output: [ 'chips', 'dip', 'cookies', 'granola' ]
-
-
-// Write the code that will return a subset of the array containing only "chips" and "dip".
-groceryList.pop()
-// console.log(groceryList)
-// output: [ 'chips', 'dip' ]
+// // var myOtherArray = ["granola"]
+// // myOtherArray.concat(groceryList)
+// // // console.log(groceryList.concat(myOtherArray))
+// // // output: [ 'chips', 'dip', 'cookies', 'granola' ]
 
 
-// Write the code that will add "beans" to the "chips" and "dip" array.
-groceryList.push("beans")
-console.log(groceryList)
-// output: [ 'chips', 'dip', 'beans' ]
+// // // Write the code that will return a subset of the array containing only "chips" and "dip".
+// // groceryList.pop()
+// // // console.log(groceryList)
+// // // output: [ 'chips', 'dip' ]
+
+
+// // // Write the code that will add "beans" to the "chips" and "dip" array.
+// // groceryList.push("beans")
+// // console.log(groceryList)
+// // // output: [ 'chips', 'dip', 'beans' ]
+
+
+
+// // Consider the variable:
+
+//  var numbers = [2, 4, 6, 8, 10]
+// // Write the code that will add the number 0 to the beginning of the array.
+//     // numbers.reverse().push(0)
+//    //console.log(numbers.reverse())
+    
+
+// // Write the code that will add the number 12 to the end of the array.
+//     //numbers.push(12)
+//     // console.log(numbers)
+
+// // Write the code that will remove the first number from the array.
+//     //numbers.reverse().pop()
+//     //console.log(numbers.reverse())
+
+
+
+// // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+//     // console.log(learnStudents1.concat(learnStudents2))
+
+//     var myOtherNumber = [0]
+//     console.log(myOtherNumber.concat(numbers))
+    
+
+var numSet = [2, 13, 6, 8, 4, 2]
+
+
+// Write the code that finds the index of the first appearance of the number 2.
+ console.log(numSet.indexOf(2))
+
+// Write the code that finds the index of the last appearance of the number 2.
+console.log(numSet.lastIndexOf(2))
+
+// Write the code that returns the number at the third index.
+console.log(numSet[2])
