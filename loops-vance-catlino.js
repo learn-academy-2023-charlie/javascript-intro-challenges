b392ba85b10704cfef2ebaf3fb7951da4dfe4b70
RodCato <code@eliment.org>
2023-04-20 17:42:40 -0500

    first commit for loops

---
+++ loops-vance-catlino.js
@@ -0,0 +1,10 @@
+// Loops Challenges 4/20/23
+
+// Create a for loop that logs each number from 1 - 20.
+for (let i = 1; i < 21; i++)
+console.log(i)
+
+// Create a for loop that logs every other number from 1 - 20.
+// Create a for loop that logs the result of each number from 1 - 20 tripled.
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
