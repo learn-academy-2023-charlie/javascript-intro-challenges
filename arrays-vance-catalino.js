f4cd9cde657098975815fae425e94db5e17e3def
RodCato <code@eliment.org>
2023-04-20 13:26:23 -0500

    second driver

--- arrays-vance-catalino.js
+++ arrays-vance-catalino.js
@@ -19,3 +19,24 @@ var nachos = (groceryList.slice (0, 2))
 // Write the code that will add "beans" to the "chips" and "dip" array.
 nachos.push ("beans")
 console.log (nachos)
+
+// Consider the variable:
+
+// var numbers = [2, 4, 6, 8, 10]
+
+// Write the code that will add the number 0 to the beginning of the array.
+var numbers = [ 0, 2, 4, 6, 8, 10, ]
+console.log (numbers)
+
+// Write the code that will add the number 12 to the end of the array.
+numbers.push (12) 
+console.log (numbers)
+
+// Write the code that will remove the first number from the array.
+numbers.shift()
+console.log(numbers)
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var reverseNumbers = numbers.reverse().push(0)
+console.log(numbers.reverse())
+
