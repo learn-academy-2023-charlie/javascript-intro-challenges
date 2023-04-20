9339899d323fbca7c67ac6983c54973bc20dc332
kyloh1996 <kyloh1996@gmail.com>
2023-04-20 17:56:53 -0500

    fourth trial

--- Arrays-kyle-ernesto.js
+++ Arrays-kyle-ernesto.js
@@ -53,14 +53,52 @@
 //     console.log(myOtherNumber.concat(numbers))
     
 
-var numSet = [2, 13, 6, 8, 4, 2]
+// var numSet = [2, 13, 6, 8, 4, 2]
 
 
 // Write the code that finds the index of the first appearance of the number 2.
- console.log(numSet.indexOf(2))
+//  console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
-console.log(numSet.lastIndexOf(2))
+// console.log(numSet.lastIndexOf(2))
 
 // Write the code that returns the number at the third index.
-console.log(numSet[2])
+// console.log(numSet[2])
+
+
+
+ var characters = ["y", "a", "r", "r", "a"]
+// Write the code that brings all the letters in the characters array together into a string.
+    //console.log(characters.join(""))
+    //output: yarra
+
+// Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
+    var charsReversed = characters.reverse()
+    //console.log(charsReversed)
+
+// Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
+    //console.log(charsReversed.join())
+    //output: a,r,r,a,y
+// Write the code that brings all the letters in the charsReversed array together into a string without separators.
+    // console.log(charsReversed.join(""))
+    // output: array
+
+
+//  Create two arrays consisting of three first names of your cohort members in each.
+// kyle, ernesto, cherry 
+// sarah,scott,bea
+
+var myArray = ["kyle","ernesto","cherry"]
+var myOtherArray = ["sarah","scott","bea"]
+
+// Write the code that sorts the names in alphabetical order.
+
+// console.log(myArray.concat(myOtherArray).sort())
+//output: [ 'bea', 'cherry', 'ernesto', 'kyle', 'sarah', 'scott' ]
+
+// Write the code that sorts the names in reverse alphabetical order.
+
+// Write the code that sorts all the names in alphabetical order in a single array.
+
+// console.log(myArray.concat(myOtherArray).sort().reverse())
+// output: [ 'scott', 'sarah', 'kyle', 'ernesto', 'cherry', 'bea' ]
