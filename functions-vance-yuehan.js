8ff67ba51830ac28f4a18669d7063653017deaf1
LEARN Academy <lanyuke@hotmail.com>
2023-04-21 12:03:52 -0700

    completed isLonger

--- functions-vance-yuehan.js
+++ functions-vance-yuehan.js
@@ -38,9 +38,61 @@ console.log(multiply(3,4))
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
 
+//Function divisibleBy
+//input: (num1, num2)
+//output: return "num1 is evenly divisible by num2." if is evenly divisible.
+//return "num1 is not evenly divisible by num2." if is not evenly divisible.
+
+
+
+const divisibleBy = (num1, num2) => {
+    if (num1 % num2 != 0) {
+        return `${num1} is not evenly divisible by ${num2}.`
+    }
+    else {
+        return `${num1} is evenly divisible by ${num2}.`
+    }
+    }
+console.log(divisibleBy(10, 5));
+console.log(divisibleBy(9, 5));
+
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+// Function: assignGrade
+// input:num
+// output:letter
+
+const assignGrade = (num) => {
+if (num >= 80) {
+    return `score ${num} is an A!`
+} else if (num <= 60) {
+    return `score ${num} is a C!`
+} else {
+    return `score ${num} is a B!`
+}
+}
+
+console.log(assignGrade(100));
+console.log(assignGrade(50));
+console.log(assignGrade(70));
+
 
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+// Function: isLonger
+// input:("string1", "string2")
+// output: return "string1 has the most characters!" if string1 has the most characters
+// return "string2 has the most characters!" if string2 has the most characters
+const isLonger = (string1, string2) => {
+if (string1.length > string2.length) {
+    return `${string1} has the most characters!`
+} else if (string2.length > string1.length) {
+    return `${string2} has the most characters!`
+} else {
+    return `${string1} and ${string2} has the same characters amount!`
+}
+}
+console.log(isLonger("donut", "cake"));
+console.log(isLonger("tea", "coffee"));
+console.log(isLonger("fire", "tire"));
 
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
 
